3c03f2648e7dfd72dc40e0362f434ff74c59219d
Joshua Wencl <Joshw@magenic.com>
2020-12-03 10:48:58 -0600

    Adding app

---
+++ clicker/App.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace clicker
+{
+    partial class App
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.StartButton = new System.Windows.Forms.Button();
+            this.StopButton = new System.Windows.Forms.Button();
+            this.PollingTextBox = new System.Windows.Forms.TextBox();
+            this.PollingLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(46, 36);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(273, 57);
+            this.StartButton.TabIndex = 0;
+            this.StartButton.Text = "Start";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // StopButton
+            // 
+            this.StopButton.Location = new System.Drawing.Point(46, 119);
+            this.StopButton.Name = "StopButton";
+            this.StopButton.Size = new System.Drawing.Size(273, 56);
+            this.StopButton.TabIndex = 0;
+            this.StopButton.Text = "Stop";
+            this.StopButton.UseVisualStyleBackColor = true;
+            this.StopButton.Click += new System.EventHandler(this.StopButton_Click);
+            // 
+            // PollingTextBox
+            // 
+            this.PollingTextBox.Location = new System.Drawing.Point(353, 54);
+            this.PollingTextBox.Name = "PollingTextBox";
+            this.PollingTextBox.Size = new System.Drawing.Size(100, 23);
+            this.PollingTextBox.TabIndex = 1;
+            this.PollingTextBox.Text = "60000";
+            // 
+            // PollingLabel
+            // 
+            this.PollingLabel.AutoSize = true;
+            this.PollingLabel.Location = new System.Drawing.Point(353, 36);
+            this.PollingLabel.Name = "PollingLabel";
+            this.PollingLabel.Size = new System.Drawing.Size(72, 15);
+            this.PollingLabel.TabIndex = 2;
+            this.PollingLabel.Text = "Polling (MS)";
+            // 
+            // App
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 187);
+            this.Controls.Add(this.PollingLabel);
+            this.Controls.Add(this.PollingTextBox);
+            this.Controls.Add(this.StopButton);
+            this.Controls.Add(this.StartButton);
+            this.Name = "App";
+            this.Text = "Clicker";
+            this.Load += new System.EventHandler(this.App_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Button StopButton;
+        private System.Windows.Forms.TextBox PollingTextBox;
+        private System.Windows.Forms.Label PollingLabel;
+    }
+}
+
---
+++ clicker/App.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace clicker
+{
+    public partial class App : Form
+    {
+        bool clicking = false;
+        const int MOUSEEVENTF_LEFTDOWN = 2;
+        const int MOUSEEVENTF_LEFTUP = 4;
+        const int INPUT_MOUSE = 0;
+        int sleep = 60000;
+
+        [DllImport("User32.dll", SetLastError = true)]
+        public static extern int SendInput(int nInputs, ref INPUT pInputs, int cbSize);
+
+        public struct MOUSEINPUT
+        {
+            public int dx;
+            public int dy;
+            public int mouseData;
+            public int dwFlags;
+            public int time;
+            public IntPtr dwExtraInfo;
+        }
+
+        public struct INPUT
+        {
+            public uint type;
+            public MOUSEINPUT mi;
+        };
+
+
+        public void ClickCurrentLocation()
+        {
+            Task.Run(() =>
+            {
+                while (clicking)
+                {
+                    INPUT i = new INPUT();
+                    i.type = INPUT_MOUSE;
+                    i.mi.dx = 0;
+                    i.mi.dy = 0;
+                    i.mi.dwFlags = MOUSEEVENTF_LEFTDOWN;
+                    i.mi.dwExtraInfo = IntPtr.Zero;
+                    i.mi.mouseData = 0;
+                    i.mi.time = 0;
+                    SendInput(1, ref i, Marshal.SizeOf(i));
+                    i.mi.dwFlags = MOUSEEVENTF_LEFTUP;
+                    SendInput(1, ref i, Marshal.SizeOf(i));
+                    System.Threading.Thread.Sleep(sleep);
+                }
+            });
+
+        }
+
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            clicking = true;
+            sleep = int.Parse(PollingTextBox.Text);
+            ClickCurrentLocation();
+        }
+
+        private void StopButton_Click(object sender, EventArgs e)
+        {
+            clicking = false;
+        }
+
+        private void App_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
